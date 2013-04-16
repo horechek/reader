4706e44ec58807690065330be092569509377a9a
horechek <horechek@gmail.com>
2013-04-17 00:57:15 +0300

    подправли оформление кода

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,14 +1,18 @@
 from feeds.models import *
 from django.contrib import admin
 
+
 class FeedAdmin(admin.ModelAdmin):
     pass
+
 
 class FeedItemAdmin(admin.ModelAdmin):
     pass
 
+
 class TagAdmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(Feed, FeedAdmin)
 admin.site.register(FeedItem, FeedItemAdmin)
--- feeds/views.py
+++ feeds/views.py
@@ -1,11 +1,12 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.utils import simplejson 
+from django.utils import simplejson
 from django .http import HttpResponse
 
 from feeds.forms import *
 from feeds.models import *
+
 
 @login_required
 def add_feed(request):
@@ -16,28 +17,31 @@ def add_feed(request):
         form = FeedForm()
     return render(request, 'feeds/add.html', {'form': form})
 
+
 @login_required
 def remove_feed(request, id):
     pass
+
 
 @login_required
 def main(request):
     feeds = Feed.objects.filter(user=request.user.id)
     return render(request, 'feeds/main.html', {'feeds': feeds})
 
+
 def load_items(request, feed_id):
     items = FeedItem.objects.filter(feed=feed_id)
     json = toJSON(items)
     return HttpResponse(json, mimetype='application/json')
 
+
 def load_item_content(request, item_id):
     item = FeedItem.objects.get(pk=item_id)
     # json = toJSON(item)
-    return HttpResponse(simplejson.dumps({
-                    'content': item.summary,
-                    'title': item.title
-                }), 
-                mimetype='application/json')
+    return HttpResponse(simplejson.dumps({'content': item.summary,
+                                          'title': item.title}),
+                        mimetype='application/json')
+
 
 def set_item_read(request):
     pass
@@ -50,6 +54,6 @@ def toJSON(obj):
         #do the same as above by making it a queryset first
         set_obj = [obj]
         set_str = simplejson.dumps(simplejson.loads(serialize('json', set_obj)))
-        #eliminate brackets in the beginning and the end 
+        #eliminate brackets in the beginning and the end
         str_obj = set_str[1:len(set_str)-2]
     return str_obj
