3ae786848c92a041f39a68b101dd5fbf4e34413a
horechek <horechek@gmail.com>
2013-04-17 00:57:15 +0300

    подправли оформление кода

--- feeds/forms.py
+++ feeds/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from feeds.models import *
 
+
 class FeedForm(forms.ModelForm):
     class Meta:
         model = Feed
--- feeds/models.py
+++ feeds/models.py
@@ -6,6 +6,7 @@ from django.utils.simplejson import dumps, loads, JSONEncoder
 from django.db.models.query import QuerySet
 from django.utils.functional import curry
 
+
 class DjangoJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, QuerySet):
@@ -13,7 +14,8 @@ class DjangoJSONEncoder(JSONEncoder):
             # structure, the easiest way is to load the JSON
             # string produced by `serialize` and return it
             return loads(serialize('json', obj))
-        return JSONEncoder.default(self,obj)
+        return JSONEncoder.default(self, obj)
+
 
 # Create your models here.
 class Tag(models.Model):
@@ -23,16 +25,18 @@ class Tag(models.Model):
     def __unicode__(self):
         return self.title
 
+
 class Feed(models.Model):
-    url = models.CharField(max_length = 255)
+    url = models.CharField(max_length=255)
     title = models.CharField(max_length=255, blank=True)
     description = models.TextField(max_length=255, blank=True)
     link = models.CharField(max_length=255, blank=True)
     user = models.ForeignKey(User)
     tags = models.ManyToManyField(Tag, verbose_name="tags", related_name="tags", blank=True)
-    
+
     def __unicode__(self):
         return self.url
+
 
 class FeedItem(models.Model):
     feed = models.ForeignKey(Feed)
