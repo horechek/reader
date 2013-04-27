f7181e926628b6607e1f0a131dbbe1ce981b51f9
horechek <horechek@gmail.com>
2013-04-27 22:59:41 +0300

    переделал команду. разбил на функции

--- feeds/management/commands/updatefeeds.py
+++ feeds/management/commands/updatefeeds.py
@@ -24,6 +24,7 @@ import re
 # feedparser.registerDateHandler(myDateHandler)
 # http://wiki.python.org/moin/RssLibraries
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for user in User.objects.all():
@@ -32,62 +33,69 @@ class Command(BaseCommand):
 
 def update_feeds_by_user(user):
     for feed in Feed.objects.filter(user=user):
-        # feed = Feed.objects.get(pk=874)
-        # # url = 'http://feeds.feedburner.com/AntonShevchuk'
-        f = feedparser.parse(feed.url)
-        if 'title' in f['channel']:
-            feed.title = f['channel']['title']
-        else:
-            feed.title = feed.url
-        if 'description' in f['channel']:
-            feed.description = f['channel']['description']
-        if 'link' in f['channel']:
-            feed.link = f['channel']['link']
-        else:
-            feed.link = feed.url
-        feed.save()
+        rss_feed = feedparser.parse(feed.url)
+        feed = set_new_data_in_feed(feed, rss_feed)
         print feed.title + ":" + feed.url
-        if 'items' in f:
-            for item in f['items']:
-                try:
-                    article = FeedItem.objects.get(link=item['link'])
-                    update = True
-                except:
-                    article = FeedItem()
-                    update = False
+        if 'items' in rss_feed:
+            for rss_item in rss_feed['items']:
+                set_new_data_in_feed_item(feed. rss_item)
 
-                if update:
-                    continue
-                try:
-                        # print item
-                    if 'link' in item:
-                        # print 'item link: ' + item['link']
-                        article.link = item['link']
-                    if 'title' in item:
-                        article.title = item['title']
-                    else:
-                        article.title = item['link']
-                    if 'date_parsed' in item:
-                        d = datetime.fromtimestamp(mktime(item['date_parsed']))
-                        article.date = d.strftime("%Y-%m-%d")
-                    elif 'published_parsed' in item:
-                        d = datetime.fromtimestamp(mktime(item['published_parsed']))
-                        article.date = d.strftime("%Y-%m-%d")
-                        # article.date = item['date']
-                    elif not update:
-                        d = date.today()
-                        article.date = d.strftime("%Y-%m-%d")
 
-                    print article.date
+def set_new_data_in_feed(feed, rss_feed):
+    if 'title' in rss_feed['channel']:
+        feed.title = rss_feed['channel']['title']
+    else:
+        feed.title = feed.url
+    if 'description' in rss_feed['channel']:
+        feed.description = rss_feed['channel']['description']
+    if 'link' in rss_feed['channel']:
+        feed.link = rss_feed['channel']['link']
+    else:
+        feed.link = feed.url
+    feed.save()
+    return feed
 
-                    if 'content' in item:
-                        article.content = item['content']
-                    if 'summary' in item:
-                        article.summary = item['summary']
-                        # if 'date' in item['summary']:
-                        #     print item['summary']['date']
-                    article.feed_id = feed.id
-                    article.shortDescr = strip_tags(article.summary)[0:45] + '...'
-                    article.save()
-                except Exception, e:
-                    pass
+
+def set_new_data_in_feed_item(feed, rss_item):
+    try:
+        article = FeedItem.objects.get(link=rss_item['link'])
+        update = True
+    except:
+        article = FeedItem()
+        update = False
+
+    if update:
+        return
+    try:
+            # print rss_item
+        if 'link' in rss_item:
+            # print 'rss_item link: ' + rss_item['link']
+            article.link = rss_item['link']
+        if 'title' in rss_item:
+            article.title = rss_item['title']
+        else:
+            article.title = rss_item['link']
+        if 'date_parsed' in rss_item:
+            d = datetime.fromtimestamp(mktime(rss_item['date_parsed']))
+            article.date = d.strftime("%Y-%m-%d")
+        elif 'published_parsed' in rss_item:
+            d = datetime.fromtimestamp(mktime(rss_item['published_parsed']))
+            article.date = d.strftime("%Y-%m-%d")
+            # article.date = rss_item['date']
+        elif not update:
+            d = date.today()
+            article.date = d.strftime("%Y-%m-%d")
+
+        print article.date
+
+        if 'content' in rss_item:
+            article.content = rss_item['content']
+        if 'summary' in rss_item:
+            article.summary = rss_item['summary']
+            # if 'date' in rss_item['summary']:
+            #     print rss_item['summary']['date']
+        article.feed_id = feed.id
+        article.shortDescr = strip_tags(article.summary)[0:45] + '...'
+        article.save()
+    except Exception, e:
+        pass
