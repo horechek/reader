a133b014c46c3cc196c24b01443991bb5e496652
horechek <horechek@gmail.com>
2013-04-28 21:39:48 +0300

    настройка тасков

--- feeds/tasks.py
+++ feeds/tasks.py
@@ -8,25 +8,25 @@ from feeds.management.commands.updatefeeds import set_new_data_in_feed
 from feeds.management.commands.updatefeeds import add_new_item_to_feed
 
 
-@task
+@task()
 def update_feeds(user):
     update_feeds_by_user(user)
 
 
-@task
+@task()
 def update_new_feed(feed):
     rss_feed = feedparser.parse(feed.url)
     set_new_data_in_feed(feed, rss_feed)
 
 
-@task
+@task()
 def add_item_to_feed(feed):
     rss_feed = feedparser.parse(feed.url)
     add_new_item_to_feed(feed, rss_feed)
 
 
 # @periodic_task(run_every=crontab(hour=0, minute=30))
-@task
+@task()
 def update_all_feeds():
     for user in User.objects.all():
             update_feeds_by_user(user)
