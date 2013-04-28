0eb20eb56d232ec257c7608f42ec24580e00100f
horechek <horechek@gmail.com>
2013-04-28 15:12:48 +0300

    добавил таск для обновления нового фида

--- feeds/tasks.py
+++ feeds/tasks.py
@@ -1,13 +1,21 @@
 from celery.decorators import task, periodic_task
 from celery.task.schedules import crontab
 from django.contrib.auth.models import User
+import feedparser
 
 from feeds.management.commands.updatefeeds import update_feeds_by_user
+from feeds.management.commands.updatefeeds import set_new_data_in_feed
 
 
 @task
 def update_feed(user):
     update_feeds_by_user(user)
+
+
+@task
+def update_new_feed(feed):
+    rss_feed = feedparser.parse(feed.url)
+    set_new_data_in_feed(feed, rss_feed)
 
 
 # @periodic_task(run_every=crontab(hour=0, minute=30))
