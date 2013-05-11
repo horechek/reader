4faa7c4fdf7dd9831cd63cb8ea3693635cc5b1cf
horechek <horechek@gmail.com>
2013-05-11 16:19:05 +0300

    Страница настроек фидов и тегов

--- feeds/templatetags/feed_form.py
+++ feeds/templatetags/feed_form.py
@@ -1,22 +1,39 @@
 from django import template
 from django.middleware.csrf import get_token
 from feeds.forms import *
+from feeds.models import *
 
 
 register = template.Library()
 
 
 def add_feed_form(parser, token):
-    return FeedFormNode()
+    try:
+        tag_name, feed = token.split_contents()
+        return FeedFormNode(feed)
+    except ValueError:
+        return FeedFormNode()
 
 
 class FeedFormNode(template.Node):
-    def render(self, context):
-        form = FeedForm()
-        t = template.loader.get_template('feeds/widgets/feed_popup.html')
-        csrf_token = str(get_token(context['request']))
+    def __init__(self, feed=False):
+        self.feed = False
+        if feed:
+            self.feed = template.Variable(feed)
 
-        return t.render(template.Context({'form': form, 'csrf_token': csrf_token},
+    def render(self, context):
+        if not self.feed:
+            form = FeedForm()
+            t = template.loader.get_template('feeds/widgets/feed_popup.html')
+            action = '/feeds/add/'
+        else:
+            feed = self.feed.resolve(context)
+            # feed = Feed.objects.get(id=feed_id)
+            form = FeedForm(instance=feed)
+            t = template.loader.get_template('feeds/feed_form.html')
+            action = '/feeds/edit/'+str(feed.id)+'/'
+        csrf_token = str(get_token(context['request']))
+        return t.render(template.Context({'form': form, 'csrf_token': csrf_token, 'action': action},
                         autoescape=context.autoescape))
 
 register.tag('feed_form', add_feed_form)
--- feeds/templatetags/tag_form.py
+++ feeds/templatetags/tag_form.py
@@ -1,21 +1,38 @@
 from django import template
 from django.middleware.csrf import get_token
 from feeds.forms import *
+from feeds.models import *
 
 
 register = template.Library()
 
 
 def add_tag_form(parser, token):
-    return TagFormNode()
+    try:
+        tag_name, tag = token.split_contents()
+        return TagFormNode(tag)
+    except ValueError:
+        return TagFormNode()
 
 
 class TagFormNode(template.Node):
+    def __init__(self, tag=False):
+        self.tag = False
+        if tag:
+            self.tag = template.Variable(tag)
+
     def render(self, context):
-        form = TagForm()
-        t = template.loader.get_template('feeds/widgets/tag_popup.html')
+        if not self.tag:
+            form = TagForm()
+            t = template.loader.get_template('feeds/widgets/tag_popup.html')
+            action = '/feeds/add_tag/'
+        else:
+            tag = self.tag.resolve(context)
+            form = TagForm(instance=tag)
+            t = template.loader.get_template('feeds/tag_form.html')
+            action = '/feeds/edit_tag/'+str(tag.id) + '/'
         csrf_token = str(get_token(context['request']))
-        return t.render(template.Context({'form': form, 'csrf_token': csrf_token},
+        return t.render(template.Context({'form': form, 'csrf_token': csrf_token, 'action': action},
                         autoescape=context.autoescape))
 
 register.tag('tag_form', add_tag_form)
